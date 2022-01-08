cf95d05fbdfd6eaae81e831ce9a521441ad4f021
Sam Hindess <mail@sohcah.dev>
2022-01-08 16:47:17 +0000

    Basic Renderer and Controls
    Co-authored-by: Paul Hindess <BeetrootChessElf@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,228 @@
-import { useState } from 'react'
-import './App.css'
-import './transformations'
+import { useState } from "react";
+import "./App.css";
+import "./transformations";
+import {
+  applyTransformation,
+  Coord,
+  FlipType,
+  generateGrid,
+  RotateType,
+  TransformationType,
+} from "./transformations";
+import type { Grid } from "./transformations";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function GridComponent({
+  grid,
+  onCellClick,
+  borders,
+}: {
+  grid: Grid;
+  onCellClick?: (row: number, column: number) => void;
+  borders?: {
+    top: number;
+    bottom: number;
+    left: number;
+    right: number;
+  };
+}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Transformation Game!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="grid">
+      {grid.map((row, rowIndex) => (
+        <div className="row" key={rowIndex}>
+          {row.map((cell, cellIndex) => (
+            <div
+              onClick={() => {
+                onCellClick?.(rowIndex, cellIndex);
+              }}
+              className={getCellClasses(rowIndex, cellIndex, borders)}
+              key={cellIndex}>
+              <div
+                className="cellinner"
+                style={{ backgroundColor: ["#ff7777", "#ffff77", "#77ff77", "#7777ff"][cell] }}
+              />
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+function App() {
+  const [grid, setGrid] = useState(generateGrid(8, 8, 4));
+
+  const [positions, setPositions] = useState<Coord[]>([]);
+
+  const canMove = positions.length >= 2 && (positions[0][0] !== positions[1][0] || positions[0][1] !== positions[1][1]);
+  const canSquareMove =
+    canMove &&
+    Math.abs(positions[0][0] - positions[1][0]) === Math.abs(positions[0][1] - positions[1][1]);
+  const canFlipX = canMove && positions[0][0] !== positions[1][0];
+  const canFlipY = canMove && positions[0][1] !== positions[1][1];
+
+  return (
+    <div>
+      <GridComponent
+        grid={grid}
+        borders={
+          positions.length === 0
+            ? undefined
+            : {
+                top: Math.min(...positions.map(p => p[0])),
+                bottom: Math.max(...positions.map(p => p[0])),
+                left: Math.min(...positions.map(p => p[1])),
+                right: Math.max(...positions.map(p => p[1])),
+              }
+        }
+        onCellClick={(row, col) => {
+          if (positions.length >= 2) {
+            setPositions([[row, col]]);
+          } else {
+            setPositions([...positions, [row, col]]);
+          }
+        }}
+      />
+      {canMove && (
+        <div>
+          {canFlipX && <button
+            onClick={() => {
+              setPositions([]);
+              setGrid(
+                applyTransformation(
+                  grid,
+                  positions[0],
+                  positions[1],
+                  TransformationType.Flip,
+                  FlipType.X
+                )
+              );
+            }}>
+            Flip X
+          </button>}
+          {canFlipY && <button
+            onClick={() => {
+              setPositions([]);
+              setGrid(
+                applyTransformation(
+                  grid,
+                  positions[0],
+                  positions[1],
+                  TransformationType.Flip,
+                  FlipType.Y
+                )
+              );
+            }}>
+            Flip Y
+          </button>}
+          {canSquareMove && (
+            <>
+              <button
+                onClick={() => {
+                  setPositions([]);
+                  setGrid(
+                    applyTransformation(
+                      grid,
+                      positions[0],
+                      positions[1],
+                      TransformationType.Flip,
+                      FlipType.Pos
+                    )
+                  );
+                }}>
+                Flip +
+              </button>
+              <button
+                onClick={() => {
+                  setPositions([]);
+                  setGrid(
+                    applyTransformation(
+                      grid,
+                      positions[0],
+                      positions[1],
+                      TransformationType.Flip,
+                      FlipType.Neg
+                    )
+                  );
+                }}>
+                Flip -
+              </button>
+              <button
+                onClick={() => {
+                  setPositions([]);
+                  setGrid(
+                    applyTransformation(
+                      grid,
+                      positions[0],
+                      positions[1],
+                      TransformationType.Rotate,
+                      RotateType.R90
+                    )
+                  );
+                }}>
+                Rotate 90
+              </button>
+            </>
+          )}
+          <button
+            onClick={() => {
+              setPositions([]);
+              setGrid(
+                applyTransformation(
+                  grid,
+                  positions[0],
+                  positions[1],
+                  TransformationType.Rotate,
+                  RotateType.R180
+                )
+              );
+            }}>
+            Rotate 180
+          </button>
+          {canSquareMove && (
+            <button
+              onClick={() => {
+                setPositions([]);
+                setGrid(
+                  applyTransformation(
+                    grid,
+                    positions[0],
+                    positions[1],
+                    TransformationType.Rotate,
+                    RotateType.R270
+                  )
+                );
+              }}>
+              Rotate 270
+            </button>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
+function getCellClasses(
+  rowIndex: number,
+  cellIndex: number,
+  borders: { top: number; bottom: number; left: number; right: number } | undefined
+): string | undefined {
+  const classes = ["cell"];
+
+  if (borders) {
+    if (rowIndex === borders.top && cellIndex >= borders.left && cellIndex <= borders.right) {
+      classes.push("cellbordertop");
+    }
+    if (rowIndex === borders.bottom && cellIndex >= borders.left && cellIndex <= borders.right) {
+      classes.push("cellborderbottom");
+    }
+    if (cellIndex === borders.left && rowIndex >= borders.top && rowIndex <= borders.bottom) {
+      classes.push("cellborderleft");
+    }
+    if (cellIndex === borders.right && rowIndex >= borders.top && rowIndex <= borders.bottom) {
+      classes.push("cellborderright");
+    }
+  }
+
+  return classes.join(" ");
+}
--- src/transformations.ts
+++ src/transformations.ts
@@ -24,9 +24,9 @@ export function flipX(grid: Grid, [top, left]: Coord, [bottom, right]: Coord): G
     newGrid[r] = [];
     for (let c = 0; c < grid[0].length; c++) {
       if (r >= top && r <= bottom && c >= left && c <= right) {
-        newGrid[r][c] = grid[top+bottom - r][c];
+        newGrid[r][c] = grid[top + bottom - r][c];
       } else {
-        newGrid[r][c] = grid[r][c]
+        newGrid[r][c] = grid[r][c];
       }
     }
   }
@@ -42,7 +42,7 @@ export function flipY(grid: Grid, [top, left]: Coord, [bottom, right]: Coord): G
       if (r >= top && r <= bottom && c >= left && c <= right) {
         newGrid[r][c] = grid[r][left + right - c];
       } else {
-        newGrid[r][c] = grid[r][c]
+        newGrid[r][c] = grid[r][c];
       }
     }
   }
@@ -58,7 +58,7 @@ export function flipPos(grid: Grid, [top, left]: Coord, [bottom, right]: Coord):
       if (r >= top && r <= bottom && c >= left && c <= right) {
         newGrid[r][c] = grid[top + right - c][left + bottom - r];
       } else {
-        newGrid[r][c] = grid[r][c]
+        newGrid[r][c] = grid[r][c];
       }
     }
   }
@@ -74,7 +74,7 @@ export function flipNeg(grid: Grid, [top, left]: Coord, [bottom, right]: Coord):
       if (r >= top && r <= bottom && c >= left && c <= right) {
         newGrid[r][c] = grid[top + c - left][left + r - top];
       } else {
-        newGrid[r][c] = grid[r][c]
+        newGrid[r][c] = grid[r][c];
       }
     }
   }
@@ -82,8 +82,7 @@ export function flipNeg(grid: Grid, [top, left]: Coord, [bottom, right]: Coord):
   return newGrid;
 }
 
-
-export function rotate90C(grid: Grid, [top, left]: Coord, [bottom, right]: Coord): Grid {
+export function rotate90(grid: Grid, [top, left]: Coord, [bottom, right]: Coord): Grid {
   const newGrid: Grid = [];
   for (let r = 0; r < grid.length; r++) {
     newGrid[r] = [];
@@ -91,7 +90,7 @@ export function rotate90C(grid: Grid, [top, left]: Coord, [bottom, right]: Coord
       if (r >= top && r <= bottom && c >= left && c <= right) {
         newGrid[r][c] = grid[top + right - c][left + r - top];
       } else {
-        newGrid[r][c] = grid[r][c]
+        newGrid[r][c] = grid[r][c];
       }
     }
   }
@@ -99,7 +98,7 @@ export function rotate90C(grid: Grid, [top, left]: Coord, [bottom, right]: Coord
   return newGrid;
 }
 
-export function rotate90A(grid: Grid, [top, left]: Coord, [bottom, right]: Coord): Grid {
+export function rotate270(grid: Grid, [top, left]: Coord, [bottom, right]: Coord): Grid {
   const newGrid: Grid = [];
   for (let r = 0; r < grid.length; r++) {
     newGrid[r] = [];
@@ -107,7 +106,7 @@ export function rotate90A(grid: Grid, [top, left]: Coord, [bottom, right]: Coord
       if (r >= top && r <= bottom && c >= left && c <= right) {
         newGrid[r][c] = grid[top + c - left][left + bottom - r];
       } else {
-        newGrid[r][c] = grid[r][c]
+        newGrid[r][c] = grid[r][c];
       }
     }
   }
@@ -123,10 +122,80 @@ export function rotate180(grid: Grid, [top, left]: Coord, [bottom, right]: Coord
       if (r >= top && r <= bottom && c >= left && c <= right) {
         newGrid[r][c] = grid[top + bottom - r][left + right - c];
       } else {
-        newGrid[r][c] = grid[r][c]
+        newGrid[r][c] = grid[r][c];
       }
     }
   }
   renderGrid(newGrid);
   return newGrid;
+}
+
+export enum TransformationType {
+  Flip,
+  Rotate,
+}
+
+export enum FlipType {
+  X,
+  Y,
+  Pos,
+  Neg,
+}
+
+export enum RotateType {
+  R90,
+  R180,
+  R270,
+}
+
+export function applyTransformation(
+  grid: Grid,
+  pos1: Coord,
+  pos2: Coord,
+  type: TransformationType.Flip,
+  value: FlipType
+): Grid;
+export function applyTransformation(
+  grid: Grid,
+  pos1: Coord,
+  pos2: Coord,
+  type: TransformationType.Rotate,
+  value: RotateType
+): Grid;
+export function applyTransformation(
+  grid: Grid,
+  pos1: Coord,
+  pos2: Coord,
+  type: TransformationType,
+  value: FlipType | RotateType
+): Grid {
+  const topLeft: Coord = [Math.min(pos1[0], pos2[0]), Math.min(pos1[1], pos2[1])];
+  const bottomRight: Coord = [Math.max(pos1[0], pos2[0]), Math.max(pos1[1], pos2[1])];
+
+  switch (type) {
+    case TransformationType.Flip:
+      switch (value) {
+        case FlipType.X:
+          return flipX(grid, topLeft, bottomRight);
+        case FlipType.Y:
+          return flipY(grid, topLeft, bottomRight);
+        case FlipType.Pos:
+          return flipPos(grid, topLeft, bottomRight);
+        case FlipType.Neg:
+          return flipNeg(grid, topLeft, bottomRight);
+      }
+      break;
+    case TransformationType.Rotate:
+      switch (value) {
+        case RotateType.R90:
+          return rotate90(grid, topLeft, bottomRight);
+        case RotateType.R180:
+          return rotate180(grid, topLeft, bottomRight);
+        case RotateType.R270:
+          return rotate270(grid, topLeft, bottomRight);
+      }
+      break;
+  }
+
+  throw new Error("Invalid transformation type");
 }
